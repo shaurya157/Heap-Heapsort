51a5188709cbc26d79498d7b77c97a5cb1b0a345
apple <shaurya157@gmail.com>
2016-11-28 19:43:18 -0800

    bugfixes

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,13 +2,13 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    2.upto(count).each do |heap_sz|
-      BinaryMinHeap.heapify_up(self, heap_sz - 1, heap_sz)
+    2.upto(count).each do |heap_size|
+      BinaryMinHeap.heapify_up(self, heap_size - 1, heap_size)
     end
 
-    count.downto(2).each do |heap_sz|
-      self[heap_sz - 1], self[0] = self[0], self[heap_sz - 1]
-      BinaryMinHeap.heapify_down(self, 0, heap_sz - 1)
+    count.downto(2).each do |heap_size|
+      self[heap_size - 1], self[0] = self[0], self[heap_size - 1]
+      BinaryMinHeap.heapify_down(self, 0, heap_size - 1)
     end
 
     self.reverse!
